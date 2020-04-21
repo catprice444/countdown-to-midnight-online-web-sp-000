82e29661506e276a8f30c71e1eff7e7ee229b6fd
Catherine Price <catprice444@gmail.com>
2020-04-21 20:22:12 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -3,8 +3,11 @@
 def countdown(number)
   while number > 0
     puts "#{number} SECOND(S)!"
-    sleep 5
     number -= 1
   end
   "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep
+  sleep 1
 end
