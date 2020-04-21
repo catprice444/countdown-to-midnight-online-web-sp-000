72cd8d8b1e0c5212880ad913802fec77b3f1fc6e
Catherine Price <catprice444@gmail.com>
2020-04-21 20:23:03 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,13 +1,9 @@
 #write your code here
 
 def countdown(number)
-  while number > 0
+  sleep 1 while number > 0
     puts "#{number} SECOND(S)!"
     number -= 1
   end
   "HAPPY NEW YEAR!"
-end
-
-def countdown_with_sleep
-  sleep 1
 end
